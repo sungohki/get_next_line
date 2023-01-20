c4a867f8b4f2fad07baca0883c7180e4ff2a10dc
Sungoh Kim <sungohki@c6r1s7.42seoul.kr>
2023-01-20 17:26:47 +0900

    modified

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sungohki <sungohki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/31 15:52:01 by sungohki          #+#    #+#             */
-/*   Updated: 2023/01/17 05:08:04 by sungohki         ###   ########.fr       */
+/*   Updated: 2023/01/20 17:19:56 by sungohki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,25 @@ char	*get_next_line(int fd)
 	static int		flag = -2;
 	static int		cursor = 0;
 	static char		temp[BUFFER_SIZE + 1];
-	char			*result;
-	char			*sub;
+	char			*result[2];
 
 	if (flag == -2 || cursor >= flag)
 		if (init(&flag, &cursor, temp, fd) == 0)
 			return (NULL);
-	result = malloc_line(temp, cursor);
+	result[0] = malloc_line(temp, cursor);
 	while (is_endofline(temp, cursor) == -1)
 	{
 		if (init(&flag, &cursor, temp, fd) == 0)
 			break ;
-		sub = result;
-		result = ft_strjoin(sub, temp);
-		free(sub);
+		result[1] = result[0];
+		result[0] = ft_strjoin(result[1], temp);
+		free(result[1]);
 	}
 	if (flag == -1)
 	{
-		free(result);
+		free(result[0]);
 		return (NULL);
 	}
 	cursor += line_len(temp, cursor);
-	return (result);
+	return (result[0]);
 }
